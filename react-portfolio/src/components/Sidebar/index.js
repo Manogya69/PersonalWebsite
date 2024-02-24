fab699f44b8aa05ac224acd05d7a4dcc4eef3015
Manogya <manogya57@gmail.com>
2024-02-24 19:36:52 +0545

    created a sidebar component

---
+++ react-portfolio/src/components/Sidebar/index.js
@@ -0,0 +1,71 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubstitute from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { faHome, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub, faTwitter,  } from '@fortawesome/free-brands-svg-icons'
+
+
+const Sidebar = () => (
+  <div className="nav-bar">
+    {/* <Link className="logo" to="/">
+      <img src={LogoS} alt="logo" />
+      <img className="sub-logo" src={LogoSubstitute} alt="subtitle" />
+    </Link> */}
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/manogya-manandhar-060b14225/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/Manogya69"
+        >
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://twitter.com/manogya69"
+        >
+          <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
+  </div>
+)
+
+export default Sidebar
