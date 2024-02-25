c1793ea2662c061bedbea62ce2b12d19814f88a7
Manogya <manogya57@gmail.com>
2024-02-25 21:09:24 +0545

    ...

--- react-portfolio/src/components/Layout/index.js
+++ react-portfolio/src/components/Layout/index.js
@@ -2,7 +2,11 @@ import './index.scss'
 import Sidebar from '../Sidebar'
 
 const Layout = () => {
-    // return <><Sidebar></Sidebar></>
+    return (
+      <>
+        <Sidebar></Sidebar>
+      </>
+    )
 }
 
 export default Layout
