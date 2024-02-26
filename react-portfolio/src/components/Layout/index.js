a33c03788508eab467e7628e5e7d6da0029248ca
Manogya <manogya57@gmail.com>
2024-02-26 20:15:20 +0545

    added places to display content and its css

--- react-portfolio/src/components/Layout/index.js
+++ react-portfolio/src/components/Layout/index.js
@@ -3,9 +3,20 @@ import Sidebar from '../Sidebar'
 
 const Layout = () => {
     return (
-      <>
+      <div className="App">
         <Sidebar></Sidebar>
-      </>
+        <div className="page">
+          <span className="tags top-tags">&tl;body&gt;</span>
+
+          {/* <Outlet /> */}
+
+          <span className="tags bottom-tags">
+            &lt;/body&gt;
+            <br />
+            <span className="bottom-tag-html">&lt;/html&gt;</span>
+          </span>
+        </div>
+      </div>
     )
 }
 
