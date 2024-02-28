0ec0379085f86c0e9b82f005df8e76160b386b37
Manogya <manogya57@gmail.com>
2024-02-28 16:02:09 +0545

    created Home page with styling

---
+++ react-portfolio/src/components/Home/index.js
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/logo-s.png'
+import './index.scss'
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hi, <br /> I'm <img src={LogoTitle} alt="developer" />
+          Manogya Manandhar
+          <br />
+          Web Developer
+        </h1>
+        <h2>FrontEnd Developer / </h2>
+        <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Home
