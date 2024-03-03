f398ce56d57cea3bb8149705442b850d1cef4e21
Manogya <manogya57@gmail.com>
2024-03-03 22:16:35 +0545

    changed home page content

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -3,47 +3,77 @@ import LogoTitle from '../../assets/images/logo-s.png'
 import './index.scss'
 import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
+import Loader from 'react-loaders'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
-  const nameArray = ['a', 'n', 'o', 'g', 'y', 'a']
-  const jobArray = ['W', 'e', 'b',' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
+  const nameArray = ['a', 'n', 'o', 'g', 'y', 'a', ',']
+  const jobArray = [
+    'A',
+    ' ',
+    'W',
+    'e',
+    'b',
+    ' ',
+    'D',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+  ]
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       setLetterClass('text-animate-hover')
     }, 4000)
-    return () => clearTimeout(timeoutId);
+    return () => clearTimeout(timeoutId)
   }, [])
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={LogoTitle} alt="developer" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={22}
-          />
-        </h1>
-        <h2>FrontEnd Developer / </h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>e</span>
+            <span className={`${letterClass} _13`}>l</span>
+            <span className={`${letterClass} _14`}>l</span>
+            <span className={`${letterClass} _15`}>o</span>
+            <span className={`${letterClass} _16`}> </span>
+            <span className={`${letterClass} _17`}>W</span>
+            <span className={`${letterClass} _18`}>o</span>
+            <span className={`${letterClass} _19`}>r</span>
+            <span className={`${letterClass} _20`}>l</span>
+            <span className={`${letterClass} _21`}>d</span>
+            <span className={`${letterClass} _22`}>,</span>
+            <br />
+            <span className={`${letterClass} _23`}>I</span>
+            <span className={`${letterClass} _24`}>'m</span>
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={25}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={14}
+            />
+          </h1>
+          <h2>FrontEnd Developer / Graphic Designer / SEO Specialist</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 
